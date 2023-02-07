b2d7de34252ccbc7b656f8d161f2c2138d02aea1
Schuler <Schuler@C-SCHULER>
2023-02-07 10:29:30 +0100

    Add new Class

---
+++ WinPeOperator/PackageManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Dism;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinPeOperator
+{
+    internal class PackageManager
+    {
+        private bool _alreadyInitialzed = false;
+        private DismLogLevel _dismLogLevel = DismLogLevel.LogErrors;
+        private void InitClass()
+        {
+            if (!_alreadyInitialzed)
+            {
+                DismApi.Initialize(_dismLogLevel);
+                _alreadyInitialzed = true;
+            }
+        }
+        public void InstallDotNetThree()
+        {
+            if
+            DismApi.Initialize(DismLogLevel.LogErrors);
+        }
+    }
+}
